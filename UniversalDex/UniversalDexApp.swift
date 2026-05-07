2b9138c1e1c88d65a51de3f6d0c3fdb480c8ffb9
Dylan de Groot <81420234+DylDeKrokodil@users.noreply.github.com>
2026-05-07 18:53:45 +0200

    Initial commit

---
+++ UniversalDex/UniversalDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalDexApp.swift
+//  UniversalDex
+//
+//  Created by Dylan de Groot on 05/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct UniversalDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
